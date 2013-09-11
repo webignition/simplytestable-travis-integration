a9caccc9d03f793c12a043070e844bc2be3d81c3
Jon Cram <jon@webignition.net>
2013-09-11 14:29:51 +0100

    Install opensp before perl modules

--- travis/install-html-validator.sh
+++ travis/install-html-validator.sh
@@ -1,12 +1,12 @@
 #!/bin/sh  
 # Install the W3C HTML validator
 cd ~/travis-tmp
-#wget http://switch.dl.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz
-#tar -xzf OpenSP-1.5.2.tar.gz
-#cd ~/travis-tmp/OpenSP-*
-#./configure --quiet --enable-http --disable-doc-build
-#make --silent
-#make --silent install
+wget http://switch.dl.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz
+tar -xzf OpenSP-1.5.2.tar.gz
+cd ~/travis-tmp/OpenSP-*
+./configure --quiet --enable-http --disable-doc-build
+make --silent
+make --silent install
 
 wget http://github.com/downloads/petdance/tidyp/tidyp-1.04.tar.gz
 tar -xzf tidyp-1.04.tar.gz
