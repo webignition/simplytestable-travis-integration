f83f7209437e5443aab26c75f1c8d27b2d36b8c9
Jon Cram <jon@webignition.net>
2013-09-11 14:04:15 +0100

    Try failing cpan quickly

--- travis/install-html-validator.sh
+++ travis/install-html-validator.sh
@@ -1,11 +1,11 @@
 #!/bin/sh  
 # Install the W3C HTML validator
-cd ~/travis-tmp
-wget http://switch.dl.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz
-tar -xzf OpenSP-1.5.2.tar.gz
-cd ~/travis-tmp/OpenSP-*
-./configure --quiet --enable-http --disable-doc-build
-make --silent
-make --silent install
+#cd ~/travis-tmp
+#wget http://switch.dl.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz
+#tar -xzf OpenSP-1.5.2.tar.gz
+#cd ~/travis-tmp/OpenSP-*
+#./configure --quiet --enable-http --disable-doc-build
+#make --silent
+#make --silent install
 
 printf "yes\n" | cpan Bundle::W3C::Validator
