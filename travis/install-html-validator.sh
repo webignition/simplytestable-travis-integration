8a28687c5708239228460490212c0f8df151350c
Jon Cram <jon@webignition.net>
2013-09-11 12:22:21 +0100

    Make opensp install a bit quieter

--- travis/install-html-validator.sh
+++ travis/install-html-validator.sh
@@ -4,6 +4,6 @@ cd ~/travis-tmp
 wget http://switch.dl.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz
 tar -xzf OpenSP-1.5.2.tar.gz
 cd ~/travis-tmp/OpenSP-*
-./configure --enable-http --disable-doc-build
-make
-make install
+./configure --quiet --enable-http --disable-doc-build
+make --silent
+make --silent install
