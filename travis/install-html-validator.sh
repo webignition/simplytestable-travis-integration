2c6c427eb33f4b35988e7fc9415e103fff9389f7
Jon Cram <jon@webignition.net>
2013-09-11 15:57:33 +0100

    Retrieve validator and dtd-bundle source

--- travis/install-html-validator.sh
+++ travis/install-html-validator.sh
@@ -1,5 +1,7 @@
 #!/bin/sh  
 # Install the W3C HTML validator
+
+# Install OpenSP dependency
 cd ~/travis-tmp
 wget http://switch.dl.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz
 tar -xzf OpenSP-1.5.2.tar.gz
@@ -8,6 +10,10 @@ cd ~/travis-tmp/OpenSP-*
 make --silent
 make --silent install
 
+cd ~/travis-tmp
+rm -Rf OpenSP*
+
+# Install tidyp dependency
 wget http://github.com/downloads/petdance/tidyp/tidyp-1.04.tar.gz
 tar -xzf tidyp-1.04.tar.gz
 cd tidyp-1.04
@@ -16,5 +22,14 @@ make --silent
 make --silent install
 ldconfig
 
+cd ~/travis-tmp
+rm -Rf tidyp*
+
+# Install required perl modules
 curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 cpanm Bundle::W3C::Validator
+
+# Retrieve validator source and put it where it belongs
+mkdir /usr/local/validator && cd /usr/local/validator
+wget http://validator.w3.org/validator.tar.gz && tar -xzf validator.*
+wget http://validator.w3.org/sgml-lib.tar.gz && tar -xzf sgml.*
