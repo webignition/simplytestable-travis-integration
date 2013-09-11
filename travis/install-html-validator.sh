231092fc2d6e282fbfd23804fc56952f92a21cca
Jon Cram <jon@webignition.net>
2013-09-11 14:20:47 +0100

    Install tidyp before prereq perl modules

--- travis/install-html-validator.sh
+++ travis/install-html-validator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh  
 # Install the W3C HTML validator
-#cd ~/travis-tmp
+cd ~/travis-tmp
 #wget http://switch.dl.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz
 #tar -xzf OpenSP-1.5.2.tar.gz
 #cd ~/travis-tmp/OpenSP-*
@@ -8,6 +8,13 @@
 #make --silent
 #make --silent install
 
-#printf "yes\n" | cpan Bundle::W3C::Validator
+wget http://github.com/downloads/petdance/tidyp/tidyp-1.04.tar.gz
+tar -xzf tidyp-1.04.tar.gz
+cd tidyp-1.04
+./configure --quiet
+make --silent
+make --silent install
+ldconfig
+
 curl -L http://cpanmin.us | perl - --sudo App::cpanminus
 cpanm Bundle::W3C::Validator
