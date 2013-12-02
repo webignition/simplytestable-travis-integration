ef793e68d83ec8c118c3764dbf67ce98a5f32f98
Jon Cram <jon@webignition.net>
2013-12-02 14:31:02 +0000

    Test connecting to html5 validator service

--- travis/install-html-validator.sh
+++ travis/install-html-validator.sh
@@ -59,7 +59,7 @@ export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/ && python build/build.py tes
 
 # Start up the HTML5 validator service and try connecting to it to test that it works
 cd /usr/local/html5-validator && python build/build.py --control-port=8889 run &
-curl -I http://localhost:8888
+curl -v http://localhost:8888
 
 
 # Configure validator
