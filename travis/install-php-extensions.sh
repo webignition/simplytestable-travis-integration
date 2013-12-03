2cbb1b0175483a2980dbba8fc86e5b1136e8b31d
Jon Cram <jon@webignition.net>
2013-12-03 14:12:23 +0000

    Test installing required php extensions

---
+++ travis/install-php-extensions.sh
@@ -0,0 +1,11 @@
+#!/bin/sh  
+# Note to self: Travis project root is /home/travis/build/<repo>
+# e.g. /home/travis/build/webignition/simplytestable-travis-integration
+# Install required PHP extensions
+
+# Install memcache extension and verify installation
+wget http://pecl.php.net/get/memcache-2.2.7.tgz
+tar -zxf memcache-2.2.7.tgz
+sh -c "cd memcache-2.2.7 && phpize && ./configure && make --quiet && sudo make --quiet install"
+echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`  
+php -r 'new \Memcache;'
