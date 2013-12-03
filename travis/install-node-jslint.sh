306bf74f09269c0e2011d92b398a58fcf8049789
Jon Cram <jon@webignition.net>
2013-12-03 10:22:12 +0000

    Check installed versions of node and npm

--- travis/install-node-jslint.sh
+++ travis/install-node-jslint.sh
@@ -3,19 +3,16 @@
 # e.g. /home/travis/build/webignition/simplytestable-travis-integration
 # Install node-jslint
 
-# Packaged node already present?
-nodejs -v
-
 # Install nodejs
-#sudo apt-get install python-software-properties python g++ make
-#echo "yes" | sudo add-apt-repository ppa:chris-lea/node.js
-#sudo apt-get update
-#sudo apt-get install nodejs 
-#sudo mkdir /usr/share/node-jslint && cd /usr/share/node-jslint && sudo npm install jslint@0.2.0
+sudo apt-get install python-software-properties python g++ make
+echo "yes" | sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs 
+sudo mkdir /usr/share/node-jslint && cd /usr/share/node-jslint && sudo npm install jslint@0.2.0
 
 # Check installed node and npm versions
-#node -v
-#npm -v
+node -v
+npm -v
 
 # See if npm is available
 
