618a699e5cffd025c6f784947935e0e0fb4b7cf6
Jon Cram <jon@webignition.net>
2013-12-03 10:33:09 +0000

    Test nodejs

--- travis/install-node-jslint.sh
+++ travis/install-node-jslint.sh
@@ -10,12 +10,5 @@ sudo apt-get update
 sudo apt-get install nodejs 
 sudo mkdir /usr/share/node-jslint && cd /usr/share/node-jslint && sudo npm install jslint@0.2.0
 
-# Check installed node and npm versions
-node -v
-npm -v
-
-# See if npm is available
-
-# Check installed node version (did we get the latest, it should have npm but the latest build said otherwise)
-#node -v
-#npm -v
+# Test node-jslint
+/usr/bin/nodejs /usr/share/node-jslint/node_modules/jslint/bin/jslint.js /home/travis/build/webignition/simplytestable-travis-integration/travis/sample.js
