961f47189e234c6c807c9ab99e94f30f1407715c
Jon Cram <jon@webignition.net>
2013-12-03 16:34:44 +0000

    Use full path to PHP

--- travis/install-php-extensions.sh
+++ travis/install-php-extensions.sh
@@ -2,14 +2,13 @@
 # Note to self: Travis project root is /home/travis/build/<repo>
 # e.g. /home/travis/build/webignition/simplytestable-travis-integration
 # Install required PHP extensions
-/home/travis/.phpenv/shims/php -v
+# /home/travis/.phpenv/shims/php -v
 
 # Install memcache extension and verify installation
-#sudo apt-get update
-#sudo apt-get install php5-dev
-#wget http://pecl.php.net/get/memcache-2.2.7.tgz
-#tar -zxf memcache-2.2.7.tgz
-#sh -c "cd memcache-2.2.7 && phpize && ./configure && make --quiet && sudo make --quiet install"
-#echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`  
-#php -r 'new \Memcache;'
-#which php
+sudo apt-get update
+sudo apt-get install php5-dev
+wget http://pecl.php.net/get/memcache-2.2.7.tgz
+tar -zxf memcache-2.2.7.tgz
+sh -c "cd memcache-2.2.7 && phpize && ./configure && make --quiet && sudo make --quiet install"
+echo "extension=memcache.so" >> `/home/travis/.phpenv/shims/php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`  
+/home/travis/.phpenv/shims/php -r 'new \Memcache;'
