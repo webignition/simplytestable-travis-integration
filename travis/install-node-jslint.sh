a7aabd4c71a88e8f6b69b2b366891917c26b4565
Jon Cram <jon@webignition.net>
2013-12-03 10:16:30 +0000

    node things 001

--- travis/install-node-jslint.sh
+++ travis/install-node-jslint.sh
@@ -3,11 +3,19 @@
 # e.g. /home/travis/build/webignition/simplytestable-travis-integration
 # Install node-jslint
 
+# Packaged node already present?
+node -v
+
 # Install nodejs
-sudo apt-get install python-software-properties python g++ make 
-sudo add-apt-repository ppa:chris-lea/node.js
+#sudo apt-get install python-software-properties python g++ make
+#echo "yes" | sudo add-apt-repository ppa:chris-lea/node.js
 #sudo apt-get update
-#sudo apt-get install nodejs
+#sudo apt-get install nodejs 
+#sudo mkdir /usr/share/node-jslint && cd /usr/share/node-jslint && sudo npm install jslint@0.2.0
+
+# Check installed node and npm versions
+#node -v
+#npm -v
 
 # See if npm is available
 
