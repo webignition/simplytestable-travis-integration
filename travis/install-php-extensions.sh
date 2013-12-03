e5ef9a5f0a450875678ea7f7459222d2a1f6ff4c
Jon Cram <jon@webignition.net>
2013-12-03 14:36:28 +0000

    Investigating lack of 'php' command

--- travis/install-php-extensions.sh
+++ travis/install-php-extensions.sh
@@ -10,4 +10,5 @@ wget http://pecl.php.net/get/memcache-2.2.7.tgz
 tar -zxf memcache-2.2.7.tgz
 sh -c "cd memcache-2.2.7 && phpize && ./configure && make --quiet && sudo make --quiet install"
 echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`  
-php -r 'new \Memcache;'
+#php -r 'new \Memcache;'
+which php
