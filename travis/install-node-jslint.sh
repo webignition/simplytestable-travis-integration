610ef3161846570839798b01bd9e3aeb2d6977b6
Jon Cram <jon@webignition.net>
2013-12-03 10:14:12 +0000

    Not certain that nodejs ppa is being added

--- travis/install-node-jslint.sh
+++ travis/install-node-jslint.sh
@@ -4,13 +4,13 @@
 # Install node-jslint
 
 # Install nodejs
-sudo apt-get install python-software-properties python g++ make --assume-yes
-sudo add-apt-repository ppa:chris-lea/node.js --assume-yes
-sudo apt-get update --assume-yes
-sudo apt-get install nodejs --assume-yes
+sudo apt-get install python-software-properties python g++ make 
+sudo add-apt-repository ppa:chris-lea/node.js
+#sudo apt-get update
+#sudo apt-get install nodejs
 
 # See if npm is available
 
 # Check installed node version (did we get the latest, it should have npm but the latest build said otherwise)
-node -v
+#node -v
 #npm -v
