fceca3e0305283f8f99c05d68064a97046e32cbe
Jon Cram <jon@webignition.net>
2013-12-10 21:34:21 +0000

    Fix core app dir name

--- travis/install/simplytestable/core-app/install.sh
+++ travis/install/simplytestable/core-app/install.sh
@@ -2,7 +2,7 @@
 # Note to self: Travis project root is /home/travis/build/<repo>
 # e.g. /home/travis/build/webignition/simplytestable-travis-integration
 # Install SimplyTestable core application
-mkdir ~/simplytestable/app.simplytestable.com && cd ~/simplytestable/app.simplytestable.com 
+mkdir ~/simplytestable/app.integration.simplytestable.com && cd ~/simplytestable/app.integration.simplytestable.com 
 
 git clone https://github.com/webignition/app.simplytestable.com.git .
 
