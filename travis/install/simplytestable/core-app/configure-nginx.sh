9303e03758ba6c25e3cf1fda18bd800ee8209770
Jon Cram <jon@webignition.net>
2013-12-10 19:38:32 +0000

    Fix nginx host filename typo

--- travis/install/simplytestable/core-app/configure-nginx.sh
+++ travis/install/simplytestable/core-app/configure-nginx.sh
@@ -3,7 +3,7 @@
 # e.g. /home/travis/build/webignition/simplytestable-travis-integration
 # Create nginx config for core app
 
-sudo cp /home/travis/build/webignition/simplytestable-travis-integration/travis/config/nginx/sites-available/app.simplytestable.com.conf /etc/nginx/sites-available
+sudo cp /home/travis/build/webignition/simplytestable-travis-integration/travis/config/nginx/sites-available/app.integration.simplytestable.com.conf /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/app.integration.simplytestable.com.conf /etc/nginx/sites-enabled/app.integration.simplytestable.com.conf
 
 ls -la /etc/nginx/sites-available
