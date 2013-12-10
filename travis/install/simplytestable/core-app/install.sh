7e3aae2863c10c756af33eba2797a15cae4190ef
Jon Cram <jon@webignition.net>
2013-12-10 11:40:21 +0000

    Have a bash at fully installing core app

--- travis/install/simplytestable/core-app/install.sh
+++ travis/install/simplytestable/core-app/install.sh
@@ -5,3 +5,12 @@
 mkdir ~/simplytestable/app.simplytestable.com && cd ~/simplytestable/app.simplytestable.com 
 
 git clone https://github.com/webignition/app.simplytestable.com.git .
+
+mkdir app/cache/prod && chmod -R 0777 app/cache/prod
+touch app/logs/prod.log && chmod 0777 app/logs/prod.log
+
+cp /home/travis/build/webignition/simplytestable-travis-integration/travis/config/core-app/app/config/parameters.yml app/config/parameters.yml
+echo | /home/travis/build/webignition/simplytestable-travis-integration/composer.phar install
+/home/travis/.phpenv/shims/php app/console doctrine:database:create
+/home/travis/.phpenv/shims/php app/console doctrine:migrations:migrate --no-interaction
+/home/travis/.phpenv/shims/php app/console doctrine:fixtures:load --append
