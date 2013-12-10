49650624005ca8ee8210f576760712ad04a33d6f
Jon Cram <jon@webignition.net>
2013-12-10 16:10:09 +0000

    test nginx host

--- travis/install/simplytestable/core-app/set-hosts-entry.sh
+++ travis/install/simplytestable/core-app/set-hosts-entry.sh
@@ -2,6 +2,4 @@
 # Note to self: Travis project root is /home/travis/build/<repo>
 # e.g. /home/travis/build/webignition/simplytestable-travis-integration
 
-sudo cat /etc/hosts
 echo "127.0.0.1 app.integration.simplytestable.com" | sudo tee --append /etc/hosts
-sudo cat /etc/hosts
