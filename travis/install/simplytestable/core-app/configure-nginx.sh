ca18513c87163516cf1b385d6eafab6ea7a1af97
Jon Cram <jon@webignition.net>
2013-12-10 19:30:16 +0000

    Check that /etc/nginx/sites-* contains what is expected

--- travis/install/simplytestable/core-app/configure-nginx.sh
+++ travis/install/simplytestable/core-app/configure-nginx.sh
@@ -3,6 +3,9 @@
 # e.g. /home/travis/build/webignition/simplytestable-travis-integration
 # Create nginx config for core app
 
-sudo cp /home/travis/build/webignition/simplytestable-travis-integration/travis/config/nginx/sites-available/app.simplytestable.com /etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available/app.simplytestable.com /etc/nginx/sites-enabled/app.simplytestable.com
+sudo cp /home/travis/build/webignition/simplytestable-travis-integration/travis/config/nginx/sites-available/app.simplytestable.com.conf /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/app.integration.simplytestable.com.conf /etc/nginx/sites-enabled/app.integration.simplytestable.com.conf
+
+ls -la /etc/nginx/sites-available
+ls -la /etc/nginx/sites-enabled
 sudo service nginx reload
